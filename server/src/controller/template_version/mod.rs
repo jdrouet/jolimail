187141c476a5cb19d2c14b3275c230b3ad814847
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-09-18 21:20:39 +0200

    server: remove unused endpoint
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- server/src/controller/template_version/mod.rs
+++ server/src/controller/template_version/mod.rs
@@ -1,6 +1,5 @@
 pub mod create;
 pub mod delete;
 pub mod get;
-pub mod get_content;
 pub mod list;
 pub mod update;
--- server/src/main.rs
+++ server/src/main.rs
@@ -31,7 +31,6 @@ macro_rules! bind_services {
         $app.service(controller::status::handler)
             .service(controller::template_version::create::handler)
             .service(controller::template_version::get::handler)
-            .service(controller::template_version::get_content::handler)
             .service(controller::template_version::list::handler)
             .service(controller::template_version::update::handler)
             .service(controller::template_version::delete::handler)
