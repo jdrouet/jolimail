7de6e02f284ff7b393a588a71dba7883fbde7490
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-10-17 18:09:31 +0200

    client: lint and build on ci
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- client/src/component/template-version-list-item/index.tsx
+++ client/src/component/template-version-list-item/index.tsx
@@ -1,8 +1,8 @@
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
