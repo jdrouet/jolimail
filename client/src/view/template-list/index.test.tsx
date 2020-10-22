b038af025f486fcadbbd142faa410e73f6d1956d
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-10-22 06:42:37 +0200

    test(client): test TemplateList view
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ client/src/view/template-list/index.test.tsx
@@ -0,0 +1,84 @@
+import {
+  findByAltText,
+  findByTestId,
+  fireEvent,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import nock from 'nock';
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { SWRConfig, cache } from 'swr';
+
+import View from './index';
+
+beforeEach(() => {
+  nock.cleanAll();
+  cache.clear();
+});
+
+test('empty case', async () => {
+  const scope = nock('http://localhost').get('/api/templates').once().reply(200, []);
+  const { container } = render(
+    <SWRConfig value={{ dedupingInterval: 0 }}>
+      <MemoryRouter initialEntries={['/']}>
+        <Route path="/">
+          <View key="empty case" />
+        </Route>
+        <Route path="/templates/create">
+          <div data-testid="create-view" />
+        </Route>
+        <Route path="/templates/:templateId">
+          <div data-testid="template-view" />
+        </Route>
+      </MemoryRouter>
+    </SWRConfig>,
+  );
+  await waitForElementToBeRemoved(() => container.querySelector('#loading'));
+  await findByAltText(container, 'empty template list');
+  const button = container.querySelector('button[data-action="create-template"]');
+  fireEvent.click(button!);
+  await findByTestId(container, 'create-view');
+  expect(scope.isDone()).toBeTruthy();
+});
+
+test('with some values', async () => {
+  const scope = nock('http://localhost')
+    .get('/api/templates')
+    .once()
+    .reply(200, [
+      {
+        id: 'first',
+        slug: 'first-template',
+        name: 'First template',
+        description: 'Whatever',
+        createdAt: new Date().toString(),
+        updatedAt: new Date().toString(),
+        deleteAt: null,
+      },
+    ]);
+  const { container } = render(
+    <SWRConfig value={{ dedupingInterval: 0 }}>
+      <MemoryRouter initialEntries={['/']}>
+        <Route path="/">
+          <View key="with values" />
+        </Route>
+        <Route path="/templates/create">
+          <div data-testid="create-view" />
+        </Route>
+        <Route path="/templates/:templateId">
+          <div data-testid="template-view" />
+        </Route>
+      </MemoryRouter>
+    </SWRConfig>,
+  );
+  await waitForElementToBeRemoved(() => container.querySelector('#loading'));
+  const createButton = await waitFor(() => container.querySelector('button[data-action="create-template"]'));
+  expect(createButton).toBeVisible();
+  const templates = await waitFor(() => container.querySelectorAll('[data-template]'));
+  expect(templates.length).toBeGreaterThan(0);
+  fireEvent.click(templates[0]!);
+  await findByTestId(container, 'template-view');
+  expect(scope.isDone()).toBeTruthy();
+});
