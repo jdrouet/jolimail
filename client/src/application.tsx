1c6e7c10e74d6d1c0c86ee3f635b6200882bdcf4
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-10-28 17:32:44 +0100

    refactor(client): move getter to SWRConfig
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- client/src/application.tsx
+++ client/src/application.tsx
@@ -2,26 +2,30 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { Route, HashRouter as Router, Switch } from 'react-router-dom';
+import { config as swrConfig } from 'src/service/server';
 import { theme } from 'src/theme';
 import TemplateCreateView, { ROUTE as TEMPLATE_CREATE_PATH } from 'src/view/template-create';
 import TemplateEditionView, { ROUTE as TEMPLATE_EDITION_PATH } from 'src/view/template-edition';
 import TemplateListView, { ROUTE as TEMPLATE_LIST_PATH } from 'src/view/template-list';
 import TemplateVersionCreateView, { ROUTE as TEMPLATE_VERSION_CREATE_PATH } from 'src/view/template-version-create';
 import TemplateVersionEditionView, { ROUTE as TEMPLATE_VERSION_EDITION_PATH } from 'src/view/template-version-edition';
+import { SWRConfig } from 'swr';
 
 function Application() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Switch>
-          <Route path={TEMPLATE_CREATE_PATH} component={TemplateCreateView} />
-          <Route path={TEMPLATE_VERSION_CREATE_PATH} component={TemplateVersionCreateView} />
-          <Route path={TEMPLATE_VERSION_EDITION_PATH} component={TemplateVersionEditionView} />
-          <Route path={TEMPLATE_EDITION_PATH} component={TemplateEditionView} />
-          <Route path={TEMPLATE_LIST_PATH} component={TemplateListView} />
-        </Switch>
-      </Router>
+      <SWRConfig value={swrConfig}>
+        <Router>
+          <Switch>
+            <Route path={TEMPLATE_CREATE_PATH} component={TemplateCreateView} />
+            <Route path={TEMPLATE_VERSION_CREATE_PATH} component={TemplateVersionCreateView} />
+            <Route path={TEMPLATE_VERSION_EDITION_PATH} component={TemplateVersionEditionView} />
+            <Route path={TEMPLATE_EDITION_PATH} component={TemplateEditionView} />
+            <Route path={TEMPLATE_LIST_PATH} component={TemplateListView} />
+          </Switch>
+        </Router>
+      </SWRConfig>
     </ThemeProvider>
   );
 }
