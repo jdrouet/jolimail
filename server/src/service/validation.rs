5c1cad874aab21fb9810dd740419d33a9b7d8829
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-11-21 13:39:08 +0100

    feat(server): update jsonschema dependency
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- server/src/service/validation.rs
+++ server/src/service/validation.rs
@@ -3,7 +3,7 @@ use jsonschema::JSONSchema;
 use serde_json::Value as JsonValue;
 
 pub fn validate_json_schema(params: &JsonValue) -> Result<(), ServerError> {
-    if let Err(error) = JSONSchema::compile(params, None) {
+    if let Err(error) = JSONSchema::compile(params) {
         debug!("unable to validate json schema {:?}", error);
         return Err(ServerError::BadRequest("json schema format invalid".into()));
     }
