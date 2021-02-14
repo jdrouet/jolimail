af1ba7c202b0b4de786a4341dc6d03fe7d1f07dc
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-02-14 14:24:42 +0100

    chore(deps): update some client dependencies
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- client/src/service/json-schema.ts
+++ client/src/service/json-schema.ts
@@ -1,15 +1,15 @@
-import ajv from 'ajv';
+import type AjvInstance from 'ajv';
 import { useEffect, useState } from 'react';
 
 export const useValidator = function () {
-  const [mod, setModule] = useState<ajv.Ajv>();
+  const [mod, setModule] = useState<AjvInstance>();
   useEffect(() => {
     import('ajv').then(({ default: Ajv }) => setModule(new Ajv())).catch(console.error);
   }, []);
   return mod;
 };
 
-export const validate = (instance: ajv.Ajv | undefined, input: string): boolean | undefined => {
+export const validate = (instance: AjvInstance | undefined, input: string): boolean | undefined => {
   if (!instance) return undefined;
   try {
     const body = JSON.parse(input);
