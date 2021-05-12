8143d62fbb399006c917a6f8b53ce3ed7fddd9d8
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-05-12 07:49:00 +0200

    refactor(client): add some tests on server service
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- client/src/service/server.test.ts
+++ client/src/service/server.test.ts
@@ -1,15 +1,38 @@
 import nock from 'nock';
 
-import { createTemplate } from './server';
+import { createTemplate, updateTemplate } from './server';
 
-test('createTemplate', async () => {
-  const scope = nock('http://localhost').post('/api/templates').reply(400, 'Bad Request');
-  try {
-    await createTemplate({ title: 'testing' });
-    expect(true).toBeFalsy();
-  } catch (err) {
-    expect(err.message).toEqual('Request failed with status code 400');
-  }
-  expect(scope.isDone()).toBeTruthy();
-  nock.cleanAll();
+afterEach(() => nock.cleanAll());
+
+describe('createTemplate', () => {
+  it('should return object', async () => {
+    const scope = nock('http://localhost').post('/api/templates').reply(200, { id: 'aaaa', title: 'Hello World' });
+    const result = await createTemplate({ title: 'testing' });
+    expect(result).toHaveProperty('id', 'aaaa');
+    expect(scope.isDone()).toBeTruthy();
+  });
+
+  it('should handle errors', async () => {
+    const scope = nock('http://localhost').post('/api/templates').reply(400, 'Bad Request');
+    try {
+      await createTemplate({ title: 'testing' });
+      expect(true).toBeFalsy();
+    } catch (err) {
+      expect(err.message).toEqual('Request failed with status code 400');
+    }
+    expect(scope.isDone()).toBeTruthy();
+  });
+});
+
+describe('updateTemplate', () => {
+  it('should handle errors', async () => {
+    const scope = nock('http://localhost').patch('/api/templates/aaaa').reply(400, 'Bad Request');
+    try {
+      await updateTemplate('aaaa', { title: 'testing' });
+      expect(true).toBeFalsy();
+    } catch (err) {
+      expect(err.message).toEqual('Request failed with status code 400');
+    }
+    expect(scope.isDone()).toBeTruthy();
+  });
 });
